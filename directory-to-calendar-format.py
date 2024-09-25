a756fc3fcbaeafe6b1aa7716bba87283b7a68974
DrakeO <drake.olejniczak@gmail.com>
2024-09-25 11:48:41 -0400

    recent employees added

--- directory-to-calendar-format.py
+++ directory-to-calendar-format.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
 # Load the CSV file
-df = pd.read_csv('Employee-Directory-csv-for-calendar-format.csv')
+df = pd.read_csv('Employee-Directory-Working.csv')
 
 # Open a text file for writing
 with open('employee_directory_calendar_format.txt', 'w') as f:
     # Iterate through the rows of the DataFrame and write each employee's information
     for index, row in df.iterrows():
-        f.write(f"First Name: {row['First Name']}\n")
-        f.write(f"Last Name: {row['Last Name']}\n")
+        f.write(f"Employee Name: {row['Employee Name']}\n")
         f.write(f"Department: {row['Department']}\n")
         f.write(f"Title: {row['Title']}\n")
         f.write(f"Employee ID Number: {row['Employee ID Number']}\n")
